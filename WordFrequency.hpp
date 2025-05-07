f90bac5d36a220d510b2ab1bc7dbbeb5fed7323f
phonycharger <157658786+phonycharger@users.noreply.github.com>
2025-05-06 23:06:15 -0700

    Update WordFrequency.hpp

--- WordFrequency.hpp
+++ WordFrequency.hpp
@@ -2,11 +2,12 @@
 #define WORD_FREQUENCY_HPP
 
 ///////////////////////// TO-DO (1) //////////////////////////////
-  /// Include necessary header files
-  /// Hint:  Include what you use, use what you include
-  ///
-  /// Do not put anything else in this section, i.e. comments, classes, functions, etc.  Only #include directives
-
+#include <unordered_map>
+#include <string>
+#include <string_view>
+#include <istream>
+#include <functional>
+#include <cstddef>
 /////////////////////// END-TO-DO (1) ////////////////////////////
 
 
@@ -31,11 +32,7 @@ class WordFrequency
   private:
     // Instance Attributes - See requirements
     ///////////////////////// TO-DO (2) //////////////////////////////
-      /// The class should have a single instance attribute of type std::unordered_map, which is the C++ Standard Library's
-      /// implementation of a hash table, to store the association of words (key) to the number of times a word occurs (value). Be
-      /// sure to provide all three types when defining the std::unordered_map, including setting std::unordered_map's third
-      /// argument to Hasher (WordFrequency's template parameter).
-
+std::unordered_map<std::string, std::size_t, Hasher> wordCounts_;
     /////////////////////// END-TO-DO (2) ////////////////////////////
 };
 
